b92db03bd9e432cd8c8850d20300f8501343a714
Mattia <matpolli93@gmail.com>
2021-05-24 17:15:11 +0200

    Seed database

---
+++ database/seeds/CommentsTableSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
+use App\Comment;
+
+class CommentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {   
+        // Selecting only published posts
+        $posts = Post::where("published", 1)->get();
+
+        // Looping on posts
+        foreach ($posts as $post) {
+
+            // Looping n times to generate comments
+            for ($i = 0; $i < rand(0, 5); $i++) {
+
+                $newComment = new Comment();
+                $newComment->post_id = $post->id;
+
+                // In case of a nullable column
+                if (rand(0, 1)) {
+                    $newComment->name = $faker->name();
+                }
+
+                $newComment->content = $faker->text();
+                $newComment->date = $faker->date();
+                $newComment->save();
+            }
+        }
+    }
+}
---
+++ database/seeds/PostsTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Post;
+use Illuminate\Support\Str;
+
+class PostsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 15; $i++) {
+
+            $newPost = new Post();
+            $newPost->title = $faker->sentence();
+            $newPost->date = $faker->date();
+            $newPost->image = $faker->imageUrl(700, 530, "blog-post", true);
+            $newPost->tagline = $faker->sentence();
+            $newPost->content = $faker->text();
+            $newPost->published = rand(0, 1);
+            $newPost->slug = Str::slug($newPost->title, "-");
+            $newPost->save();
+        }
+    }
+}
